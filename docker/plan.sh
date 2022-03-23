6c391196a699bad49f80578bdec7e99b4601b5a9
DKL <dekusdenial@hotmail.com>
2022-03-23 13:20:09 -0400

    upgrade docker to 20.10.13

--- docker/plan.sh
+++ docker/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=docker
 pkg_origin=anonymous
 pkg_description="The Docker Engine (Mirror)"
-pkg_version=19.03.8
+pkg_version=20.10.13
 pkg_maintainer="DekusDenial <dekusdenial@hotmail.com>"
 pkg_license=('Apache-2')
 pkg_source="https://download.docker.com/linux/static/stable/x86_64/${pkg_name}-${pkg_version}.tgz"
 pkg_upstream_url=https://docs.docker.com/engine/installation/binaries/
-pkg_shasum=7f4115dc6a3c19c917f8b9664d7b51c904def1c984e082c4600097433323cf6f
+pkg_shasum=39edf7c8d773939ff5e4d318ae565691a9c7e754ed768e172757e58898fb7079
 pkg_dirname=docker
 pkg_bin_dirs=(bin)
 
