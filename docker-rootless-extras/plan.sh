7d29660f9ac7a3462418260a7f9cbcc3e9aad404
DKL <dekusdenial@hotmail.com>
2022-03-23 13:22:49 -0400

    upgrade docker-rootless to 20.10.13

--- docker-rootless-extras/plan.sh
+++ docker-rootless-extras/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=docker-rootless-extras
 pkg_origin=anonymous
 pkg_description="The Docker Engine Rootless Extras (Mirror)"
-pkg_version=19.03.8
+pkg_version=20.10.13
 pkg_maintainer="DekusDenial <dekusdenial@hotmail.com>"
 pkg_license=('Apache-2')
 pkg_source="https://download.docker.com/linux/static/stable/x86_64/${pkg_name}-${pkg_version}.tgz"
 pkg_upstream_url=https://docs.docker.com/engine/installation/binaries/
-pkg_shasum=f7f15f61026fbf006cf1e719c99bd4b0c2a34dc9507f280fc77b4fa3e7eb1979
+pkg_shasum=39edf7c8d773939ff5e4d318ae565691a9c7e754ed768e172757e58898fb7079
 pkg_dirname=docker-rootless-extras
 pkg_bin_dirs=(bin)
 pkg_build_deps=(anonymous/docker)
